5462b17031eef96481d7cda6cee2720ffce3b62c
eserranor98 <118926192+eserranor98@users.noreply.github.com>
2023-03-31 02:00:32 -0600

    changed loading icon color

--- src/components/layout/LoadingPage.tsx
+++ src/components/layout/LoadingPage.tsx
@@ -1,9 +1,9 @@
-import { Logo } from "../ui/Logo";
+import { Logo } from "~/components/ui/Logo";
 
 export const LoadingPage = () => {
   return (
-    <div className="bg-accent flex h-screen w-full items-center justify-center">
-      <Logo />
+    <div className="flex h-screen w-full items-center justify-center bg-primary">
+      <Logo className="text-onPrimary" />
     </div>
   );
 };
--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,8 +1,12 @@
 import { CubeIcon } from "@heroicons/react/24/solid";
+import { cn } from "~/lib/utils";
 
-export const Logo = () => {
+interface IFProps {
+  className?: string;
+}
+export const Logo = ({ className }: IFProps) => {
   return (
-    <span className="flex items-center gap-2 font-semibold">
+    <span className={cn("flex items-center gap-2 font-semibold", className)}>
       <CubeIcon className="w-6" />
       <span>Inshare</span>
     </span>
