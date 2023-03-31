23075d27c80cdb7153f91977bc97f8883134d659
eserranor98 <118926192+eserranor98@users.noreply.github.com>
2023-03-31 02:02:14 -0600

    removed logo from loading screen

--- src/components/layout/LoadingPage.tsx
+++ src/components/layout/LoadingPage.tsx
@@ -1,9 +1,5 @@
-import { Logo } from "~/components/ui/Logo";
-
 export const LoadingPage = () => {
   return (
-    <div className="flex h-screen w-full items-center justify-center bg-primary">
-      <Logo className="text-onPrimary" />
-    </div>
+    <div className="flex h-screen w-full items-center justify-center bg-primary"></div>
   );
 };
